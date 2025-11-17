226c7a8751e8cc4c6e14d43713e716b7f64336e9
HTrung <nguyenhoangtrung26092006@gmail.com>
2025-11-17 08:11:20 +0700

    HeapSort

--- Algorithm/HeapSort.c
+++ Algorithm/HeapSort.c
@@ -27,15 +27,7 @@ void buildMaxHeap(int* a, int n){
     }
 }
 
-void printArray(int* a, int n) {
-    printf("[ ");
-    for (int i = 0; i < n; i++) {
-        printf("%d ", a[i]);
-    }
-    printf("]\n");
-}
-
-void HeapSort(int* a,int n){
+void MaxHeapSort(int* a,int n){
     buildMaxHeap(a,n);
 
     for(;n >= 1;){
@@ -43,6 +35,45 @@ void HeapSort(int* a,int n){
         n--;
         correctMaxHeapNode(a,n,0);
     }
+}
+
+void correctMinHeapNode(int* a, int n, int i) {
+    int li = 2*i + 1,
+    ri = li + 1;
+
+    int mi = i;
+    if(li < n && a[li] < a[mi]) mi = li;
+    if(ri < n && a[ri] < a[mi]) mi = ri;
+
+    if(mi == i) return;
+
+    swap(a,i,mi);
+    correctMinHeapNode(a,n,mi);
+}
+
+
+void buildMinHeap(int* a, int n) {
+    for(int i=n/2;i>=0;i--){
+        correctMinHeapNode(a,n,i);
+    }
+}
+
+
+void minHeapSort(int* a, int n) {
+    buildMinHeap(a,n);
+
+    for(int i=n-1;i>=1;i--){
+        swap(a,0,i);
+        correctMinHeapNode(a,i,0);
+    }
+}
+
+void printArray(int* a, int n) {
+    printf("[ ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", a[i]);
+    }
+    printf("]\n");
 }
 
 int main(){
@@ -55,9 +86,11 @@ int main(){
 
     printArray(a,n);
 
-    HeapSort(a,n);
+    // MaxHeapSort(a,n);
+    minHeapSort(a,n);
 
     printArray(a,n);
+    
     
     return 0;
 }
